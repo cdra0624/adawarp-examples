a2aa65f2eeb10cc8101b19b0f5902c847b9f2c52
TATSUKI ADANIYA <tatsukiadaniya@tatsuki.local>
2016-11-07 08:15:57 +0900

    change local path

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const app = express();
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/docs/index.html'));
 });
-app.use("/docs", express.static(path.join(__dirname, 'docs')));
-app.use("/01-1.videoChat", express.static(path.join(__dirname, 'docs/01-1.videoChat')));
+app.use("/", express.static(path.join(__dirname, 'docs')));
+//app.use("/docs", express.static(path.join(__dirname, 'docs')));
+/*app.use("/01-1.videoChat", express.static(path.join(__dirname, 'docs/01-1.videoChat')));
 app.use("/01-2.helloSender", express.static(path.join(__dirname, 'docs/01-2.helloSender')));
-app.use("/01-3.helloReceiver", express.static(path.join(__dirname, 'docs/01-3.helloReceiver')));
+app.use("/01-3.helloReceiver", express.static(path.join(__dirname, 'docs/01-3.helloReceiver')));*/
 
 app.use("/assets", express.static(path.join(__dirname, 'docs/assets')));
 
