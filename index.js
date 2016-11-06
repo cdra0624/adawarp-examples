df239a429942b5ed70d4c535b997233eea5555b4
TATSUKI ADANIYA <tatsukiadaniya@tatsuki.local>
2016-11-07 08:18:10 +0900

    remove comment

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/docs/index.html'));
 });
 app.use("/", express.static(path.join(__dirname, 'docs')));
-//app.use("/docs", express.static(path.join(__dirname, 'docs')));
-/*app.use("/01-1.videoChat", express.static(path.join(__dirname, 'docs/01-1.videoChat')));
-app.use("/01-2.helloSender", express.static(path.join(__dirname, 'docs/01-2.helloSender')));
-app.use("/01-3.helloReceiver", express.static(path.join(__dirname, 'docs/01-3.helloReceiver')));*/
-
 app.use("/assets", express.static(path.join(__dirname, 'docs/assets')));
 
 app.listen(PORT);
